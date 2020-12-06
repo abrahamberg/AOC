c0c27dbe6cc258711ba52d0491af00424e98888e
Daniel Abrahamberg <daniel@abrahamberg.com>
2020-12-06 12:40:57 +0100

    Day 6

--- 2020/06.cs
+++ 2020/06.cs
@@ -1,22 +1,59 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Application;
 
 namespace AdventOfCode._2020
 {
-    class _06 : Solution
+    internal class _06 : Solution
     {
         public _06(IEnumerable<string> inputs, VersionEnum version) : base(inputs, version)
-        {
-        }
+        { }
 
         protected override string SolverA(IEnumerable<string> inputs)
         {
-            throw new System.NotImplementedException();
+            return Looper.Loop(inputs).Select(x
+                    => x.Distinct().Count())
+                .Aggregate((current, member) => current + member).ToString();
         }
 
         protected override string SolverB(IEnumerable<string> inputs)
         {
-            throw new System.NotImplementedException();
+            var count = 0;
+
+            foreach (var members in Looper.Loop2(inputs).Select(x => x.Split(",")))
+                if (members.Length == 1)
+                {
+                    count += members[0].Length;
+                }
+                else
+                {
+                    var common = members[0].Intersect(members[1]);
+                    for (var i = 2; i < members.Length; i++) common = common.Intersect(members[i]);
+
+                    count += common.Count();
+                }
+
+            return count.ToString();
+        }
+
+
+        private static class Looper
+        {
+            internal static IEnumerable<string> Loop(IEnumerable<string> inputs)
+            {
+                return string.Join("",
+                        inputs.Select(x => string.IsNullOrWhiteSpace(x) ? "\n" : x))
+                    .Split("\n");
+            }
+
+            internal static IEnumerable<string> Loop2(IEnumerable<string> inputs)
+            {
+                return string.Join(",",
+                        inputs.Select(x => string.IsNullOrWhiteSpace(x) ? "\n" : x))
+                    .Split("\n")
+                    .Select(x => x[0] == ',' ? x.Remove(0, 1) : x)
+                    .Select(x => x[^1] == ',' ? x.Remove(x.Length - 1, 1) : x);
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode
     {
         private static void Main(string[] args)
         {
-            const string defaultPuzzleName = "2020-05";
+            const string defaultPuzzleName = "2020-06";
             const string defaultPuzzleVersion = "B";
 
             IUserInterface ui = new Console();
