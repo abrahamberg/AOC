8b6580072ee28f27dc1e51b6db4ff29a37674aa3
Daniel Abrahamberg <daniel.abrahamberg@prevas.se>
2020-12-08 21:30:38 +0100

    Clean up day 8

--- 2020/08.cs
+++ 2020/08.cs
@@ -8,9 +8,9 @@ namespace AdventOfCode._2020
     internal class _08 : Solution
     {
         public _08(IEnumerable<string> inputs, VersionEnum version) : base(inputs, version)
-        {
-        }
+        { }
 
+        // Puzzle  1
         protected override string SolverA(IEnumerable<string> inputs)
         {
             var currentValue = 0;
@@ -19,51 +19,52 @@ namespace AdventOfCode._2020
             var i = 0;
             while (true)
             {
-                if (all[i].value.HasValue) 
+                if (all[i].Value.HasValue)
                     return currentValue.ToString();
 
-                switch (all[i].Instrucrion)
+                switch (all[i].Instruction)
                 {
                     case "acc":
-                        currentValue += all[i].no;
-                        all[i].value = currentValue;
-                        i = i + 1;
+                        currentValue += all[i].Number;
+                        all[i].Value = currentValue;
+                        i++;
                         continue;
                     case "jmp":
-                        all[i].value = currentValue;
-                        i = i + all[i].no;
+                        all[i].Value = currentValue;
+                        i += all[i].Number;
                         continue;
                     case "nop":
-                        all[i].value = currentValue;
-                        i = i + 1;
+                        all[i].Value = currentValue;
+                        i++;
                         continue;
                     default:
                         throw new Exception();
                 }
-
             }
         }
 
+
+        // Puzzle  2
         protected override string SolverB(IEnumerable<string> inputs)
         {
-            var org = inputs.Select(x => new Model(x));
-            var all = org.ToArray();
+            var enumerable = inputs.Select(x => new Model(x));
+            var all = enumerable.ToArray();
 
             for (var i = 0; i < all.Length; i++)
-                switch (all[i].Instrucrion)
+                switch (all[i].Instruction)
                 {
                     case "acc":
                         break;
                     case "jmp":
-                        var newAll = org.ToArray();
-                        newAll[i].Instrucrion = "nop";
-                        if (CanFinish(newAll, out var val))
+                        var newAll = enumerable.ToArray(); //Make a fresh clone of IEnumerable 
+                        newAll[i].Instruction = "nop";
+                        if (IsCompetePath(newAll, out var val))
                             return val.ToString();
                         break;
                     case "nop":
-                        var newAll2 = org.ToArray();
-                        newAll2[i].Instrucrion = "jmp";
-                        if (CanFinish(newAll2, out var val2))
+                        var newAll2 = enumerable.ToArray();
+                        newAll2[i].Instruction = "jmp";
+                        if (IsCompetePath(newAll2, out var val2))
                             return val2.ToString();
                         break;
                     default:
@@ -73,7 +74,7 @@ namespace AdventOfCode._2020
             return "NON";
         }
 
-        private static bool CanFinish(Model[] models, out int finishVal)
+        private static bool IsCompetePath(Model[] models, out int finishVal)
         {
             var currentValue = 0;
             var i = 0;
@@ -84,26 +85,27 @@ namespace AdventOfCode._2020
                     finishVal = currentValue;
                     return true;
                 }
-                if (models[i].value.HasValue)
+
+                if (models[i].Value.HasValue)
                 {
                     finishVal = currentValue;
                     return false;
                 }
 
-                switch (models[i].Instrucrion)
+                switch (models[i].Instruction)
                 {
                     case "acc":
-                        currentValue += models[i].no;
-                        models[i].value = currentValue;
-                        i = i + 1;
+                        currentValue += models[i].Number;
+                        models[i].Value = currentValue;
+                        i++;
                         continue;
                     case "jmp":
-                        models[i].value = currentValue;
-                        i = i + models[i].no;
+                        models[i].Value = currentValue;
+                        i += models[i].Number;
                         continue;
                     case "nop":
-                        models[i].value = currentValue;
-                        i = i + 1;
+                        models[i].Value = currentValue;
+                        i++;
                         continue;
                     default:
                         throw new Exception();
@@ -115,15 +117,15 @@ namespace AdventOfCode._2020
         {
             public Model(string input)
             {
-                Instrucrion = input.Substring(0, 3);
-                no = int.Parse(input.Substring(3, input.Length - 3));
-                value = null;
+                Instruction = input.Substring(0, 3);
+                Number = int.Parse(input.Substring(3, input.Length - 3));
+                Value = null;
             }
 
-            public string Instrucrion { get; set; }
-            public int no { get; }
+            public string Instruction { get; set; }
+            public int Number { get; }
 
-            public int? value { get; set; }
+            public int? Value { get; set; }  //We can actually implement it with a boolean as it only check if we ever been here.
         }
     }
 }
