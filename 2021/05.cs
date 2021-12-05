b160f955ce89877b269c1cf97aa6db21dbbda1cc
Daniel Abrahamberg <daniel@abrahamberg.com>
2021-12-05 15:55:55 +0100

    performance 2021-05

--- 2021/05.cs
+++ 2021/05.cs
@@ -9,230 +9,148 @@ namespace AdventOfCode._2021
     {
         public string Puzzle1(IReadOnlyList<string> inputs)
         {
-            var log = 0;
             var lines = ParsLines(inputs);
-            List<Point> recoredPoints = new();
+            var buffer = 1000;
+            int[,] points = new int[buffer, buffer];
+
             foreach (var line in lines)
             {
-                log++;
-                Console.WriteLine(log);
-
-                if (line.Start.X == line.End.X)
+                if (line[0] == line[2])
                 {
-                    var start = line.Start.Y < line.End.Y ? line.Start.Y : line.End.Y;
-                    var end = line.Start.Y > line.End.Y ? line.Start.Y : line.End.Y;
+                    var start = line[1] < line[3] ? line[1] : line[3];
+                    var end = line[1] > line[3] ? line[1] : line[3];
 
                     for (int i = start; i <= end; i++)
                     {
-                        var currentPoint = new Point(line.Start.X, i);
-                        CountThePoint(currentPoint);
+                        CountThePoint(line[0], i);
                     }
                 }
-                else
+                else if (line[1] == line[3])
                 {
-                    if (line.Start.Y == line.End.Y)
+                    var start = line[0] < line[2] ? line[0] : line[2];
+                    var end = line[0] > line[2] ? line[0] : line[2];
+
+                    for (int i = start; i <= end; i++)
                     {
-                        var start = line.Start.X < line.End.X ? line.Start.X : line.End.X;
-                        var end = line.Start.X > line.End.X ? line.Start.X : line.End.X;
-
-                        for (int i = start; i <= end; i++)
-                        {
-                            var currentPoint = new Point(i, line.Start.Y);
-                            CountThePoint(currentPoint);
-                        }
+                        CountThePoint(i, line[1]);
                     }
                 }
 
-                void CountThePoint(Point currentPoint)
+                void CountThePoint(int x, int y)
                 {
-                    if (recoredPoints.Any(p => p == currentPoint))
-                        recoredPoints.First(p => p == currentPoint).AddCount();
-                    else
-                        recoredPoints.Add(currentPoint);
+                    points[x, y]++;
                 }
             }
 
+            int count = 0;
+            for (int i = 0; i < buffer; i++)
+                for (int j = 0; j < buffer; j++)
+                {
+                    if (points[i, j] >= 2) count++;
+                }
+            return count.ToString();
 
-
-
-
-            return (recoredPoints.Count(x => x.Count >= 2)).ToString();
         }
-
-
-
 
         public string Puzzle2(IReadOnlyList<string> inputs)
         {
 
-            var log = 0;
             var lines = ParsLines(inputs);
-            List<Point> recoredPoints = new();
+            var buffer = 1000;
+            int[,] points = new int[buffer, buffer];
+
             foreach (var line in lines)
             {
-                log++;
-                Console.WriteLine(log);
 
-                if (line.Start.X == line.End.X)
+                if (line[0] == line[2])
                 {
-                    var start = line.Start.Y < line.End.Y ? line.Start.Y : line.End.Y;
-                    var end = line.Start.Y > line.End.Y ? line.Start.Y : line.End.Y;
+                    var start = line[1] < line[3] ? line[1] : line[3];
+                    var end = line[1] > line[3] ? line[1] : line[3];
 
                     for (int i = start; i <= end; i++)
                     {
-                        var currentPoint = new Point(line.Start.X, i);
-                        CountThePoint(currentPoint);
+                        CountThePoint(line[0], i);
                     }
                 }
-                else if (line.Start.Y == line.End.Y)
+                else if (line[1] == line[3])
                 {
-                    var start = line.Start.X < line.End.X ? line.Start.X : line.End.X;
-                    var end = line.Start.X > line.End.X ? line.Start.X : line.End.X;
+                    var start = line[0] < line[2] ? line[0] : line[2];
+                    var end = line[0] > line[2] ? line[0] : line[2];
 
                     for (int i = start; i <= end; i++)
                     {
-                        var currentPoint = new Point(i, line.Start.Y);
-                        CountThePoint(currentPoint);
+                        CountThePoint(i, line[1]);
                     }
                 }
                 else if (
-                    Math.Abs( line.Start.X - line.Start.Y) == Math.Abs (line.End.X - line.End.Y)
-                    || Math.Abs(line.Start.X - line.End.Y) == Math.Abs(line.Start.Y - line.End.X)
+                    Math.Abs(line[0] - line[1]) == Math.Abs(line[2] - line[3])
+                    || Math.Abs(line[0] - line[3]) == Math.Abs(line[1] - line[2])
                     )
                 {
-                    bool isStartSmallerX = line.Start.X <= line.End.X;
-                    bool isStartSmallerY = line.Start.Y <= line.End.Y;
+                    bool isStartSmallerX = line[0] <= line[2];
+                    bool isStartSmallerY = line[1] <= line[3];
 
 
-                    Point previusPouint = null;
-                    while (previusPouint != line.End)
+                    var previusPouint = new int[2] { -1, -1 };
+                    while (!(previusPouint[0] == line[2] && previusPouint[1] == line[3]))
                     {
-                        Point currentPoint;
-                        if (previusPouint == null)
-                            currentPoint = line.Start;
+                        int[] currentPoint = new int[2];
+                        if (previusPouint[0] == -1)
+                        {
+                            currentPoint[0] = line[0];
+                            currentPoint[1] = line[1];
+                        }
                         else
                         {
-                            var x = isStartSmallerX ? previusPouint.X + 1 : previusPouint.X - 1;
-                            var y = isStartSmallerY ? previusPouint.Y + 1 : previusPouint.Y - 1;
-                            Console.WriteLine(x + "," + y);
-                            currentPoint = new(x, y);
+                            var x = isStartSmallerX ? previusPouint[0] + 1 : previusPouint[0] - 1;
+                            var y = isStartSmallerY ? previusPouint[1] + 1 : previusPouint[1] - 1;
+                            currentPoint[0] = x;
+                            currentPoint[1] = y;
 
                         }
-                       
-                        CountThePoint(currentPoint);
+                        CountThePoint(currentPoint[0], currentPoint[1]);
                         previusPouint = currentPoint;
                     }
                 }
-                
 
-                void CountThePoint(Point currentPoint)
+
+
+                void CountThePoint(int x, int y)
                 {
-                    if (recoredPoints.Any(p => p == currentPoint))
-                        recoredPoints.First(p => p == currentPoint).AddCount();
-                    else
-                        recoredPoints.Add(currentPoint);
+                    points[x, y]++;
                 }
             }
 
-            //Render
-            //var maxX = recoredPoints.Max(x => x.X);
-            //var maxY = recoredPoints.Max(y => y.Y);
-            //for (int j = 0; j <= maxY; j++)
-            //{
-            //    for (int i   = 0; i <= maxX; i++)
-            //    {
-            //        if (recoredPoints.Any(x => x.X == i && x.Y == j))
-            //            Console.Write(recoredPoints.First(x => x.X == i && x.Y == j).Count+$"[{i},{j}]");
-            //        else
-            //            Console.Write("." + $"[{i},{j}]");
-            //    }
-            //    Console.WriteLine();
-
-
-            //}
-
-            return (recoredPoints.Count(x => x.Count >= 2)).ToString();
+            int count = 0;
+            for (int i = 0; i < buffer; i++)
+                for (int j = 0; j < buffer; j++)
+                {
+                    if (points[i, j] >= 2) count++;
+                }
+            return count.ToString();
         }
 
 
-        private List<(Point Start, Point End)> ParsLines(IReadOnlyList<string> inputs)
+        private List<int[]> ParsLines(IReadOnlyList<string> inputs)
         {
-            List<(Point Start, Point End)> lines = new();
+            List<int[]> lines = new();
             foreach (var inp in inputs.Select(x => x.Split(" -> ")))
             {
                 var start = inp[0].Split(',');
                 var end = inp[1].Split(',');
 
-                Point startPoint = new(int.Parse(start[0]), int.Parse(start[1]));
-                Point endPoint = new(int.Parse(end[0]), int.Parse(end[1]));
-                var line = (startPoint, endPoint);
-                lines.Add(line);
+                int[] item = new int[4];
+                item[0] = int.Parse(start[0]);
+                item[1] = int.Parse(start[1]);
+                item[2] = int.Parse(end[0]);
+                item[3] = int.Parse(end[1]);
+                lines.Add(item);
+
             }
             return lines;
         }
 
 
-        class Point
-        {
-            public int Count { get; private set; }
-            public readonly int X;
-            public readonly int Y;
-            public Point(int x, int y)
-            {
-                X = x;
-                Y = y;
-                Count = 1;
-            }
-            public void AddCount()
-            {
-                Count++;
-            }
-            public bool Equals(Point otherPoint)
-            {
-                if (otherPoint is null)
-                {
-                    if (this is null)
-                    {
-                        return true;
-                    }
 
-                    // Only the left side is null.
-                    return false;
-                }
-                return otherPoint.X == X && otherPoint.Y == Y;
-            }
-
-            public override int GetHashCode() => (X, Y).GetHashCode();
-            public static bool operator ==(Point lp, Point rp)
-            {
-                if (lp is null)
-                {
-                    if (rp is null)
-                    {
-                        return true;
-                    }
-
-                    // Only the left side is null.
-                    return false;
-                }
-                // Equals handles case of null on right side.
-                return lp.Equals(rp);
-            }
-            public static bool operator !=(Point lp, Point rp)
-            {
-                if (lp is null)
-                {
-                    if (rp is null)
-                    {
-                        return false;
-                    }
-
-                    // Only the left side is null.
-                    return true;
-                }
-                // Equals handles case of null on right side.
-                return !lp.Equals(rp);
-            }
-        }
     }
 }
