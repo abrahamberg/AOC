9a66e848568716c56cae1e44f03951a76a9c3e01
abrahamberg <daniel@abrahamberg.com>
2021-12-06 11:37:33 +0100

    2021-06 cleanup2

--- 2021/06.cs
+++ 2021/06.cs
@@ -10,93 +10,63 @@ namespace AdventOfCode._2021
     {
         public string Puzzle1(IReadOnlyList<string> inputs)
         {
-            var countDays = new int[9];
-            var init = inputs[0].Split(',');
-            for (int i = 0; i <= 8; i++)
-            {
-               countDays[i]= init.Count(x =>  x == i.ToString());
-               
-            }
-            for (int day = 1; day <= 80; day++)
-            {
-                var newBorn =  0;
-                for (int i = 0 ;i <= 8; i++)
-                {
-                    switch (i)
-                    {
-                        case 0:
-                            newBorn = countDays[0];
-                            break;
-                        case 7:// add to day 6
-                            countDays[i-1] = countDays[i]+newBorn;
-                            break;
-                        case 8:
-                            countDays[i - 1] = countDays[i];
-                            countDays[i] = newBorn;
-                            break;
-                        default:
-                            countDays[i - 1] = countDays[i];
-                            break;
-                    }
-                }
-            }
-
-            var count = 0;
-            for (int i = 0; i <= 8; i++)
-            {
-                count = count+ countDays[i];
-            }
-        
-            
+            var timerCountAtDay = CountInitState(inputs);
+            var count = LoopThtoughDays(timerCountAtDay,80);
             return count.ToString();
-
         }
 
         public string Puzzle2(IReadOnlyList<string> inputs)
         {
+            var timerCountAtDay = CountInitState(inputs);
+            var count = LoopThtoughDays(timerCountAtDay,256);
+            return count.ToString();
+        }
 
-            var countDays = new BigInteger[9];
+        private static BigInteger[] CountInitState(IReadOnlyList<string> inputs)
+        {
+            var timerCountAtDay = new BigInteger[9];
             var init = inputs[0].Split(',');
             for (int i = 0; i <= 8; i++)
             {
-                countDays[i]= init.Count(x =>  x == i.ToString());
-               
+                timerCountAtDay[i] = init.Count(x => x == i.ToString());
             }
-            for (int day = 1; day <= 256; day++)
+
+            return timerCountAtDay;
+        }
+
+        private static BigInteger LoopThtoughDays(BigInteger[] timerCountAtDay,int days)
+        {
+            for (int day = 1; day <= days; day++)
             {
-                BigInteger newBorn =  0;
-                for (int i = 0 ;i <= 8; i++)
+                BigInteger newBorn = 0;
+                for (int i = 0; i <= 8; i++)
                 {
                     switch (i)
                     {
                         case 0:
-                            newBorn = countDays[0];
+                            newBorn = timerCountAtDay[0];
                             break;
                         case 7: // add to day 6
-                            countDays[i-1] = countDays[i]+newBorn;
+                            timerCountAtDay[i - 1] = timerCountAtDay[i] + newBorn;
                             break;
                         case 8:
-                            countDays[i - 1] = countDays[i];
-                            countDays[i] = newBorn;
+                            timerCountAtDay[i - 1] = timerCountAtDay[i];
+                            timerCountAtDay[i] = newBorn;
                             break;
                         default:
-                            countDays[i - 1] = countDays[i];
+                            timerCountAtDay[i - 1] = timerCountAtDay[i];
                             break;
                     }
                 }
             }
+
             BigInteger count = 0;
             for (int i = 0; i <= 8; i++)
             {
-                count = count+ countDays[i];
+                count += timerCountAtDay[i];
             }
-        
-            
-            return count.ToString();
 
+            return count;
         }
-
-
-       
     }
 }
