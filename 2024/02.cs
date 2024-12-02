9aeaddc685d52e53f773ddbbba383784393dddc1
Daniel Abrahamberg <daniel@abrahamberg.com>
2024-12-02 22:18:57 +0100

    2024-02 refactor

--- 2024/02.cs
+++ 2024/02.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using AdventOfCode.Application;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.PortableExecutable;
-using AdventOfCode.Application;
 
 namespace AdventOfCode._2024;
 
@@ -10,68 +9,51 @@ namespace AdventOfCode._2024;
 
 public class _02 : ISolution
 {
-    private bool IsSafe(int[] n)
-    {
-
-        bool assending = n[1] - n[0] > 0;
-        var copy = assending ?
-            n.OrderBy(x => x).ToArray() :
-            n.OrderByDescending(x => x).ToArray();
-        var correctOrder = copy.SequenceEqual(n);
-
-        for (int i = 1; i < n.Length; i++)
-        {
-            var diff = Math.Abs(n[i] - n[i - 1]);
-            if (diff > 3 || diff == 0) return false;
-        }
-
-        return correctOrder;
-    }
-
     public string Puzzle1(IReadOnlyList<string> inputs)
     {
         var lines = inputs.Select(inp => inp.Split(' ')).ToArray();
-        int safe = 0;
-        foreach (var line in lines)
-        {
-            var numbers = line.Select(int.Parse).ToArray();
-            if (IsSafe(numbers)) safe++;
-        }
-
-        return safe.ToString();
+        return lines.Count(line =>
+                {
+                    var numbers = line.Select(int.Parse).ToArray();
+                    return IsSafe(numbers);
+                })
+                .ToString();
     }
-
-
 
     public string Puzzle2(IReadOnlyList<string> inputs)
     {
         var lines = inputs.Select(inp => inp.Split(' ')).ToArray();
-        int safe = 0;
-        foreach (var line in lines)
-        {
-            var numbers = line.Select(int.Parse).ToArray();
-            var isSafe = IsSafe(numbers);
-            if (isSafe) safe++;
-            else if (SecondChase(numbers)) safe++;
-
-        }
-
-        bool SecondChase(int[] numbers)
-        {
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                var copy = numbers.ToList();
-                copy.RemoveAt(i);
-                if (IsSafe(copy.ToArray()))
+        return lines.Count(line =>
                 {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        return safe.ToString();
+                    var numbers = line.Select(int.Parse).ToArray();
+                    return IsSafe(numbers) || SecondChase(numbers);
+                })
+                .ToString();
     }
 
-   
+
+    private static bool IsSafe(int[] numbers)
+    {
+
+        bool ascending = numbers[1] - numbers[0] > 0;
+        var sorted = ascending ?
+            numbers.OrderBy(x => x).ToArray() :
+            numbers.OrderByDescending(x => x).ToArray();
+        var correctOrder = sorted.SequenceEqual(numbers);
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            var diff = Math.Abs(numbers[i] - numbers[i - 1]);
+            if (diff > 3 || diff == 0) return false;
+        }
+        return correctOrder;
+    }
+
+
+    private static bool SecondChase(int[] numbers)
+    {
+        return Enumerable.Range(0, numbers.Length)
+                 .Select(i => numbers.Where((_, index) => index != i).ToArray()) // remove one number
+                 .Any(shorten => IsSafe(shorten));
+    }
 }
